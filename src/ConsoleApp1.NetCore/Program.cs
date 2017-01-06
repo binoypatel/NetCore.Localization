818a9e2c0e5969f6a6404dfae79c4736649a0d20
binoypatel <binoypatel@msn.com>
2017-01-06 18:14:35 -0500

    Add project files.

---
+++ src/ConsoleApp1.NetCore/Program.cs
@@ -0,0 +1,23 @@
+﻿using Contoso.Languages;
+using System;
+using System.Globalization;
+
+namespace ConsoleApp1.NetCore
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("You said {0} in {1}", LocalizedStrings.WelcomeLabel, CultureInfo.CurrentCulture.EnglishName);
+            var frenchCulture = new CultureInfo("fr-CA");
+            CultureInfo.CurrentCulture = frenchCulture;
+            CultureInfo.CurrentUICulture = frenchCulture;
+            Console.WriteLine("You said {0} in {1}", LocalizedStrings.WelcomeLabel, CultureInfo.CurrentCulture.EnglishName);
+            var germanCulture = new CultureInfo("de");
+            CultureInfo.CurrentCulture = germanCulture;
+            CultureInfo.CurrentUICulture = germanCulture;
+            Console.WriteLine("You said {0} in {1}", LocalizedStrings.WelcomeLabel, CultureInfo.CurrentCulture.EnglishName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/ConsoleApp1.NetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1.NetCore")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cd81989e-b381-4ba3-b5a2-2b0901ea860f")]
